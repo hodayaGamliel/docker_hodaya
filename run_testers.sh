371a3b97d91e5468d630304fef107eb48bd6c1a3
hodayagamliel <hodaya.gamliel@takipi.com>
2015-11-02 13:50:19 +0200

    Update run_testers.sh

--- run_testers.sh
+++ run_testers.sh
@@ -2,9 +2,21 @@
 
 # Run some testers
 
+
 # Run java statistics
 nohup ./statistics-tester-java/java_nonce_create.sh 2 1000 &
 
 # Run scala boom
-java -agentlib:TakipiAgent -jar scala-boom.jar 
+nohup java -agentlib:TakipiAgent -jar scala-boom.jar & 
+
+# Run Tomcat Embedded
+export MAVEN_OTS="-agentlib:TakipiAgent"
+cd hod-embedded ; mvn compile package
+nohup cd hod-embedded ;mvn tomcat7:run &
+
+sleep 5 
+
+curl -i http://localhost:8080/hod-embedded?err
+curl -i http://localhost:8080/hod-embedded?log
+curl -i http://localhost:8080/hod-embedded?fff
 
